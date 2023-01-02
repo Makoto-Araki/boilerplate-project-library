8e8c8a8d8ac0f1254617db1c9dcbdc7414dc6a6c
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2023-01-02 06:49:16 +0000

    2023年01月02日 - 02

--- server.js
+++ server.js
@@ -1,43 +1,68 @@
 'use strict';
 
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const cors        = require('cors');
+// Import
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
+// Load modules
+const apiRoutes = require('./routes/api.js');
+const fccTestingRoutes = require('./routes/fcctesting.js');
+const runner = require('./test-runner');
 
+// App initialize
 const app = express();
 
+// App config
 app.use('/public', express.static(process.cwd() + '/public'));
-
 app.use(cors({origin: '*'})); //USED FOR FCC TESTING PURPOSES ONLY!
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Index page (static HTML)
+// Secrets
+// const user = process.env['user']
+// const pass = process.env['pass']
+// const cluster = process.env['cluster']
+// const option = process.env['option']
+// const database = process.env['database']
+
+// Mongo_URI (old)
+// const mongo_URI = `mongodb+srv://${user}:${pass}@${cluster}/${database}?${option}`;
+
+// Mongo_URI
+const mongo_URI = process.env['DB'];
+
+// MongoDB Connect Config
+mongoose.set('strictQuery', false);
+
+// MongoDB Connect
+mongoose
+.connect(mongo_URI)
+.then(() => console.log('Database connection successed'))
+.catch((err) => console.error(err))
+
+// Index page (static HTML)
 app.route('/')
   .get(function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
   });
 
-//For FCC testing purposes
+// For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
+// Routing for API 
 apiRoutes(app);  
     
-//404 Not Found Middleware
+// 404 Not Found Middleware
 app.use(function(req, res, next) {
   res.status(404)
     .type('text')
     .send('Not Found');
 });
 
-//Start our server and tests!
+// Start our server and tests!
 const listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
   if(process.env.NODE_ENV==='test') {
@@ -53,4 +78,5 @@ const listener = app.listen(process.env.PORT || 3000, function () {
   }
 });
 
-module.exports = app; //for unit/functional testing
+// For testing
+module.exports = app;
